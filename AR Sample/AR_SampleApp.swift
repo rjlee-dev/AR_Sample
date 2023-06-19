7588e37309eb30ff8fb4c01c28436b279163570d
Richard <atomicflare@hotmail.com>
2023-06-19 11:50:06 -0400

    Initial Commit

---
+++ AR Sample/AR_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AR_SampleApp.swift
+//  AR Sample
+//
+//  Created by Atomicflare on 2023-06-19.
+//
+
+import SwiftUI
+
+@main
+struct AR_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
